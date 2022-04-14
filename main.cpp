e2d1e29900b337605436c9cd3d485ca1cc92db1a
bjke <kbjwdh@gmail.com>
2022-04-15 01:15:13 +0800

    添加ffmpeg

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <QApplication>
 #include <QTranslator>
 #include "mainWindow.h"
+#include "QDebug"
+// 纯C语言的
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
 
 int main(int argc, char *argv[]) {
 //    QCoreApplication a(argc, argv);
 //    qDebug() << "Hello World";
 //    return QCoreApplication::exec();
+    // 打印版本号
+    qDebug() << av_version_info();
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("hello world");
